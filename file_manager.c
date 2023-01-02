73d23d8f07b32e56415ef99896b2b768b0fd251d
Bahadır Sina Terzioğlu <bahadir.terzioglu@stu.fsm.edu.tr>
2023-01-03 02:23:30 +0300

    Update file_manager.c

--- file_manager.c
+++ file_manager.c
@@ -14,27 +14,15 @@
 
 void *listenme(){
 
-	int fd;
-    	char * myfifo = "/tmp/myfifo"; // FIFO file path
-    	mkfifo(myfifo, 0666);
-    	char arr1[80], arr2[80];
-    	
-    	while (1) {
-		fd = open(myfifo, O_RDONLY);
-		read(fd, arr1, sizeof(arr1));
-		printf("User2: %s\n", arr1);
-		close(fd);
-		fd = open(myfifo, O_WRONLY); // Open FIFO for write only
-		fgets(arr2, 80, stdin);
-		write(fd, arr2, strlen(arr2)+1);
-		close(fd);
-    	}
+printf("Thread is running succesfully.\n");
 return NULL;
 
 }
 
 int main(int argc, char *argv[]) {
 	char File_List[20][20];
+	int flidx;
+	int flidx2;
 	pthread_t fmt;
 	void *status;
 	int t_suc = pthread_create(&fmt, NULL,listenme,NULL);
@@ -47,7 +35,79 @@ int main(int argc, char *argv[]) {
 		perror("Thread is not join!!");
 		exit(1);
 	}
-
+	int fd;
+    	char * myfifo = "/tmp/myfifo"; // FIFO file path
+    	mkfifo(myfifo, 0666);
+    	char arr1[80];
+    	char *arr2;
+    	char *cp7 = "Exit\n";
+ 	char *cp1 = "Create";
+    	while (1) {
+		fd = open(myfifo, O_RDONLY);
+		read(fd, arr1, sizeof(arr1));
+		printf("User2: %s\n", arr1);
+		close(fd);
+		char tmp[10][10]; //A temporary matrix array is created.
+     			int i,j,count;
+    			j=0; count=0;
+    			for(i=0; i <= (strlen(arr1)); i++) {  //All entries entered by the user after myshell 
+        			if(arr1[i] == ' '|| arr1[i] == '\0'){ //are separated by spaces and stored in a temporary array.
+            				tmp[count][j]='\0';
+            				count++;  
+            				j=0;  
+        			}else {	
+            				tmp[count][j]=arr1[i];
+            				j++;
+        			}
+        		}
+        	char *cp = tmp[0]; // Values were stored for comparison.
+       		char *cp0 = tmp[1]; // Filename
+		char *cp7 = "Exit\n";
+		if (strcmp(arr1, cp7) == 0){ //exit 
+			arr2= "Exit\n";
+			fd = open(myfifo, O_WRONLY); // Open FIFO for write only
+			//fgets(arr2, 80, stdin);
+			write(fd, arr2, strlen(arr2)+1);
+			close(fd);	
+		} else if (strcmp(cp, cp1) == 0){
+			for(flidx = 0; flidx < 20; flidx++) { 
+				if(strcmp(File_List[flidx] , cp0) == 0){
+					arr2 = "This file already exists in the File_list.";
+					fd = open(myfifo, O_WRONLY); // Open FIFO for write only
+					//fgets(arr2, 80, stdin);
+					write(fd, arr2, strlen(arr2)+1);
+					close(fd);
+					break;
+				}
+			}
+			if(flidx == 20){
+				strcpy(File_List[flidx2],cp0);
+				flidx2++;
+				FILE *fp;
+				char* fName = cp0; 
+				if ((fp = fopen(fName, "a+"))) {   // File is opened and check if there is a file with "a+". 
+	  				fclose(fp);  // File is closed.
+  				} else{
+      					printf("Error: File is not open!\n");
+      					exit(1);
+  				}
+	       			arr2 = "The file name is added to the file_list and the file is created in the system.";
+				fd = open(myfifo, O_WRONLY); // Open FIFO for write only
+				//fgets(arr2, 80, stdin);
+				write(fd, arr2, strlen(arr2)+1);
+				close(fd);
+			}
+			
+		}
+		else {
+			arr2 = "Wrong command entered! Enter the correct command.\n";
+			fd = open(myfifo, O_WRONLY); // Open FIFO for write only
+			//fgets(arr2, 80, stdin);
+			write(fd, arr2, strlen(arr2)+1);
+			close(fd);
+		}
+		
+    	}
     return 0;
     
 }
